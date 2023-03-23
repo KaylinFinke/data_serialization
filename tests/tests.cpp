b7e6708eb78da8eed2eb483c7157510d4d8da914
Kaylin <kaylinfinke@gmail.com>
2023-03-23 11:14:33 -0700

    fixup tests for power and mips

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -43,16 +43,16 @@ using u16 = std::uint_least16_t;
 template <u16 N>
 using U16 = std::integral_constant<u16, N>;
 
-using u8 = std::int_least8_t;
+using u8 = std::uint_least8_t;
 template <u8 N>
 using U8 = std::integral_constant<u8, N>;
 
 template<typename... Ts>
 using B = common_platform::bitfield<Ts...>;
 
-//Tests only work on common platforms due to assumptions about 8 bit bytes etc in hardcoded constants.
-//Classes still work, but this was burdensome to write and untestable on platforms I have access to.
-static_assert(common_platform::is_common_platform);
+//Some of the transparently serializable tests check things on common platforms. These tests require an 8 bit byte
+//due to hardcoded constants.
+static_assert(common_platform::transparently_serializable<u8> and std::numeric_limits<u8>::digits == 8);
 
 static constexpr auto field1 = common_platform::bitfield<std::integral_constant<signed, 7>>{ { {std::byte{0x7B} } } };
 static_assert(field1.get<0>() == -5); //signed fast path rank < int at 0.
@@ -72,11 +72,11 @@ static constexpr auto field5 = common_platform::bitfield<std::integral_constant<
 static_assert(field5.get<1>() == static_cast<unsigned long long int>(0xAAAFFEEDDCCBBAA));
 static constexpr auto field6 = common_platform::bitfield<std::integral_constant<int, 2>, std::integral_constant<unsigned long long int, 63>>{ { { std::byte{0xA0}, std::byte{0xBA}, std::byte{0xCB}, std::byte{0xDC}, std::byte{0xED}, std::byte{0xFE}, std::byte{0xAF}, std::byte{0xAA} } } };
 static_assert(field6.get<1>() == static_cast<unsigned long long int>(0xAAAFFEEDDCCBBAA0 >> 2));
-static constexpr auto field7 = []{ std::remove_const_t<decltype(field1)> f7{}; f7.set<0>(-5); return f7; }();
+static constexpr auto field7 = [] { std::remove_const_t<decltype(field1)> f7{}; f7.set<0>(-5); return f7; }();
 static_assert(field7 == field1);
-static constexpr auto field8 = []{ std::remove_const_t<decltype(field2)> f8{}; f8.set<1>(-5); return f8; }();
+static constexpr auto field8 = [] { std::remove_const_t<decltype(field2)> f8{}; f8.set<1>(-5); return f8; }();
 static_assert(field8 == field2);
-static constexpr auto field9 = [x = field3.get<1>()]{ std::remove_const_t<decltype(field3)> f9{}; f9.set<1>(x); f9.s[8] |= std::byte{0x08};  return f9; }();
+static constexpr auto field9 = [x = field3.get<1>()] { std::remove_const_t<decltype(field3)> f9{}; f9.set<1>(x); f9.s[8] |= std::byte{ 0x08 };  return f9; }();
 static_assert(field9 == field3);
 static constexpr auto field10 = common_platform::bitfield<std::integral_constant<signed, 7>>{ { {std::byte{5} } } };
 static_assert(field10 > field1);
@@ -84,37 +84,37 @@ static constexpr auto field11 = common_platform::bitfield < std::integral_consta
 static_assert(std::to_integer<int>(field11.get<1>()) == (-5 & 0x7F));
 
 static_assert(not common_platform::is_transparently_serializable_v<bool>);
-static_assert(common_platform::is_transparently_serializable_v<i32>);
-static_assert(common_platform::is_transparently_serializable_v<const i32>);
-static_assert(common_platform::is_transparently_serializable_v<volatile i32>);
-static_assert(common_platform::is_transparently_serializable_v<const volatile i32>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<i32>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<const i32>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<volatile i32>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<const volatile i32>);
 using B1 = B<I64<32>, U8<5>, I16<7>>;
 static_assert(common_platform::is_transparently_serializable_v<B1>);
 struct T1 { B1 a; u16 b; };
-static_assert(common_platform::is_transparently_serializable_v<T1>);
-static_assert(common_platform::is_transparently_serializable_v<const T1>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T1>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<const T1>);
 struct T2 { B1 a; i16 b; u8 c; };
 static_assert(not common_platform::is_transparently_serializable_v<T2>);
 struct T3 { const B1 a; u16 b; };
 static_assert(not common_platform::is_transparently_serializable_v<T3>);
 struct T4 { u16 a; u8 b, c; alignas(u32) B<I64<64>> d; };
-static_assert(common_platform::is_transparently_serializable_v<T4>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T4>);
 struct T5 { alignas(8) B<I64<64>> a; u16 b; u8 c, d; u32 e; };
-static_assert(common_platform::is_transparently_serializable_v<T5>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T5>);
 struct T6 { alignas(4) struct { u16 a[2]; } a; u32 b; };
-static_assert(common_platform::is_transparently_serializable_v<T6>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T6>);
 struct T7 { struct { alignas(4) u16 a[2]; } a; u32 b; };
-static_assert(common_platform::is_transparently_serializable_v<T7>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T7>);
 static_assert(std::is_same_v<decltype(std::declval<B1&>().get<i64>()), i64>);
-static_assert(std::is_void_v<decltype(std::declval<B1&>().set(i16{}))>);
+static_assert(std::is_void_v<decltype(std::declval<B1&>().set(i16{})) > );
 
-static_assert(common_platform::is_transparently_serializable_v<T5, T6>);
-static_assert(common_platform::is_transparently_serializable_v<T5, T6[]>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T5, T6>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<T5, T6[]>);
 static_assert(not common_platform::is_transparently_serializable_v<B1, T5>);
 static_assert(common_platform::is_transparently_serializable_v<B1[], B1[]>);
 static_assert(not common_platform::is_transparently_serializable_v<B1[], T5>);
 static_assert(alignof(B1) < alignof(T1));
-static_assert(common_platform::is_transparently_serializable_v<B1[], T1>);
+static_assert(not common_platform::is_common_platform or common_platform::is_transparently_serializable_v<B1[], T1>);
 
 struct Foo {};
 
@@ -151,12 +151,12 @@ static_assert(std::same_as<decltype(type_conversion::strict_alias_cast<const cha
 template <typename... Ts>
 auto test() noexcept
 {
-	auto fun = [](const char*, std::uint32_t u, float p[], std::size_t n)
+	auto fun = [](const char*, std::uint32_t u, common_platform::bitfield<common_platform::float_constant<float, 24, 8, u32>> p[], std::size_t n)
 	{
-		std::cout << u << std::endl;	
-		std::for_each_n(p, n, [](auto f) { std::cout << f << std::endl; });
+		std::cout << u << std::endl;
+		std::for_each_n(p, n, [](float f) { std::cout << f << std::endl; });
 	};
-	alignas(Ts...) std::byte buf[sizeof(std::uint32_t) + sizeof(float[2])]{};
+	alignas(Ts...) std::byte buf[12]{};
 	std::memcpy(buf, "hello world", sizeof(buf));
 	return data_serialization::invoke<Ts...>(fun, std::forward_as_tuple("hello"), buf);
 }
@@ -185,15 +185,15 @@ int main()
 	assert(u.contains([] {B1 b{}; b.set<0>(1); return b; }()));
 	assert(u.contains(B1{}));
 
-	test<std::uint32_t, float[]>();
+	test<common_platform::bitfield<U32<32>>, common_platform::bitfield<common_platform::float_constant<float, 24, 8, u32>>[]>();
 
-	struct foobar { std::uint32_t a; float b[2]; };
+	struct foobar { common_platform::bitfield<U32<32>> a; common_platform::bitfield<common_platform::float_constant<float, 24, 8, u32>> b[2]; };
 	alignas(foobar) std::byte buf[sizeof(std::uint32_t) + sizeof(float[2])]{};
 
-	auto fun = [](std::uint32_t uu, float(&a)[2]) 
+	auto fun = [](std::uint32_t uu, common_platform::bitfield<common_platform::float_constant<float, 24, 8, u32>>(&a)[2])
 	{
 		std::cout << uu << std::endl;
-		std::ranges::for_each(a, [](auto ff) { std::cout << ff << std::endl; });
+		std::ranges::for_each(a, [](float ff) { std::cout << ff << std::endl; });
 		return 73;
 	}; //std::uint32_t and exactly 2 floats.
 	std::memcpy(buf, "hello world", sizeof(buf));
@@ -201,9 +201,9 @@ int main()
 	static_assert(data_serialization::flex_element_size_v<foobar, decltype(fun)> == 0);
 	assert(73 == data_serialization::apply<foobar>(fun, buf, sizeof(buf)));
 
-	auto fun2 = [](std::uint32_t uu, float p[], std::size_t n) {
+	auto fun2 = [](std::uint32_t uu, common_platform::bitfield<common_platform::float_constant<float, 24, 8, u32>> p[], std::size_t n) {
 		std::cout << uu << std::endl;
-		std::for_each_n(p, n, [](auto ff) { std::cout << ff << std::endl; });	
+		std::for_each_n(p, n, [](float ff) { std::cout << ff << std::endl; });
 	}; //std::uint32_t and 0+ floats.
 	std::memcpy(buf, "flex array!", sizeof(buf));
 	static_assert(data_serialization::apply_size_v<foobar, decltype(fun2)> == 4);
@@ -236,6 +236,4 @@ int main()
 		my_float = ffloat;
 		assert(((ifloat & 0x7FFF'FFFF) > 0x7F'FFFF) or data_serialization::from_binary32(ffloat) == data_serialization::from_binary32(float(my_float)));
 	}
-
-	return 0;
 }
